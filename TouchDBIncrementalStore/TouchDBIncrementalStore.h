11c74cb218db78d9c6b2be9165178422fc254ec9
Christian Beer <christian.beer@chbeer.de>
2013-06-02 12:35:47 +0200

    Initial Commit

---
+++ TouchDBIncrementalStore/TouchDBIncrementalStore.h
@@ -0,0 +1,13 @@
+//
+//  TouchDBIncrementalStore.h
+//  TouchDBIncrementalStore
+//
+//  Created by Christian Beer on 02.06.13.
+//  Copyright (c) 2013 Christian Beer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TouchDBIncrementalStore : NSObject
+
+@end
